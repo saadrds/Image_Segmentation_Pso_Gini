19abfa6326cabd5bf1351040c1ea3a2c80c73144
Saad Rouddasse <saadrds@gmail.com>
2021-02-09 11:41:16 +0100

    gini entropie ajoutée et la fonction qui colorise l'image

--- gini_entropy.py
+++ gini_entropy.py
@@ -1,55 +1,65 @@
+import math
+
+
 class Region:
     n: int  # number of pixel inside region
     f: float  # frequency of pixels in region
-    f_cum: float  # cumulate frequency of pixels in region
-    center: float  # centre of region
+    # f_cum: float  # cumulate frequency of pixels in region
+    centre: float  # centre of region
     gray_percent: float  # gray percent of region
     gray_percent_cum: float  # cumulate of gray centre in region
-    l1: int  # min limit
-    l2: int  # max limit
-    pixel_tab: [int]
+    l1: float  # min limit
+    l2: float  # max limit
+    width: float
+    pixel_tab: []
 
     # constructor with the region limits
     def __init__(self, l1, l2):
+        self.l1 = l1
+        self.l2 = l2
+        self.width = l1 + l2
         self.pixel_tab = [l1]
         self.pixel_tab += [l2]
 
     def add_pixel(self, pixel):  # add pixel to tab
-        self.pixel_tab += [pixel]
+        self.pixel_tab.insert(1, pixel)
 
     # pixels adding completed
     def calculate_parameters(self, total_pixels):
         self.n = len(self.pixel_tab)
         self.pixel_tab = self.pixel_tab
         self.f = self.n / total_pixels
-        self.l1 = self.pixel_tab[0]
-        self.l2 = self.pixel_tab[self.n - 1]
-        self.center = (self.l1 + self.l2) / self.n
+        self.centre = self.width / self.n
 
     # f_cum calculation
+    """
     def f_cum_cal(self, f_cum_before):
         self.f_cum = f_cum_before + self.f
         return self.f_cum
+    """
 
     # centre calculation
     def gray_percent_cum_cal(self, gray_cum_before):
         self.gray_percent_cum = gray_cum_before + self.gray_percent
+        return self.gray_percent_cum
 
 
 class Image:
-    regions: [Region]  # regions tab
+    regions: []  # regions tab
     nb_lines: int
     nb_columns: int
+    gini = 0
+    entropy = 0
 
-    def __int__(self, image_matrix, seuils, min_gray, max_gray):
+    def __init__(self, image_matrix, seuils, min_gray, max_gray):
         self.min = min_gray  # min gray value
         self.max = max_gray  # max gray value
         self.image_matrix = image_matrix  # image matrix
         self.nb_lines = len(image_matrix)
         self.nb_columns = len(image_matrix[0])
         # adding regions with limits of seuils
-        self.regions = [Region(min, seuils[0])]
-        for i in range(1, len(seuils)):
+        self.regions = [Region(min_gray, seuils[0])]
+        for i in range(len(seuils) - 1):
             self.regions += [Region(seuils[i], seuils[i + 1])]
         self.regions += [Region(seuils[len(seuils) - 1], self.max)]
         #  grouping pixels of image in propre regions
@@ -61,6 +71,26 @@ class Image:
                         self.regions[k].add_pixel(image_matrix[i][j])
                         break
         # calculating regions attributes
-        for element in self.regions:
+        # f_before = 0
+        g_before = 0
+        g2_before = 0
+        gini = 0
+        total_gray = 0
+        for i in range(len(self.regions)):
             # giving the total number of pixels to each region for calculating
-            element.calculate_parameters(self.nb_lines * self.nb_columns)
+            self.regions[i].calculate_parameters(self.nb_lines * self.nb_columns)
+            total_gray += self.regions[i].n * self.regions[i].centre
+            #  f_before = self.regions[i].f_cum_cal(f_before)
+
+        for i in range(len(self.regions)):
+            self.entropy += self.regions[i].f * math.log10(self.regions[i].f)
+            self.regions[i].gray_percent = (self.regions[i].n * self.regions[i].centre) / total_gray
+            g2_before = self.regions[i].gray_percent_cum_cal(g_before)
+            gini += self.regions[i].f * (g_before + self.regions[i].gray_percent_cum)
+            g_before = g2_before
+        self.gini = 1 - gini
+
+
+def gini_entropy(image_test, tab_pixel, min_gray, max_gray):
+    img1 = Image(image_test, tab_pixel, min_gray, max_gray)
+    return img1.gini - img1.entropy
--- main.py
+++ main.py
@@ -3,36 +3,38 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from random import *
-# import numpy
+import numpy as np
 import cv2
 import math
+import gini_entropy
 
-im_gray = cv2.imread("sources/Cameraman256.png", cv2.IMREAD_GRAYSCALE)
+im_gray = cv2.imread("sources/butterfly.jpg", cv2.IMREAD_GRAYSCALE)
 
 imageTest = [[122, 233, 213], [112, 33, 32], [13, 41, 24]]
 matrix1 = [[7, 8, 9], [4, 5, 6], [1, 2, 3]]
 imageTest2 = [[121, 232, 223], [112, 13, 32], [33, 42, 64]]
 
+colors = []
+colors += [[0, 0, 255]]  # red
+colors += [[255, 0, 0]]  # blue
+colors += [[0, 255, 0]]  # green
+colors += [[0, 255, 255]]  # yellow
+colors += [[0, 128, 255]]  # orange
+colors += [[255,149,0]]
 
-# gini entropy function
-def ge(num):
-    return num
 
-
-# function that finds the best position in the whole swarm
-def g_best_finder(image, i):
-    neighbours = []
-    # collect the neighbours of the particle i j
-    for k in range(i - 1, i + 2):
-        if k not in (-1, len(image)):
-            neighbours += [image[k]]
-    # looking for g_best in the population
-    gbest = neighbours[0]
-    element = 0
-    for element in neighbours:
-        if ge(gbest) < ge(element):
-            gbest = element
-    return element
+def draw_image(image, tab):
+    nb_lines = len(image)
+    nb_col = len(image[0])
+    colored_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    for k in range(1, len(tab)):
+        for i in range(len(image)):
+            for j in range(len(image[0])):
+                if tab[k - 1] < image[i][j] <= tab[k]:
+                    colored_image[i][j] = colors[k]
+    new_image = np.array(colored_image)
+    cv2.imshow("image segmentée tata !", colored_image)
+    cv2.waitKey()
 
 
 # psnr function (peak signal to noise ratio)
@@ -62,39 +64,62 @@ def pso(image, nb_region):
     gray_min = 255
     for i in range(len(image)):
         for j in range(len(image[0])):
-            if image[0][0] > gray_max:
-                gray_max = image[0][0]
-            if image[0][0] < gray_min:
-                gray_min = image[0][0]
+            if image[i][j] > gray_max:
+                gray_max = image[i][j]
+            if image[i][j] < gray_min:
+                gray_min = image[i][j]
 
     position_tab = [[randint(gray_min, gray_max) for i in range(nb_seuil)] for j in range(50)]
-    best_position_tab = [[position_tab[i][j] for i in range(nb_seuil)] for j in range(nb_col)]
-    velocity_tab = [[0.0 for i in range(nb_seuil)] for j in range(nb_col)]
-    g_best_tab = [[position_tab[i][j] for i in range(nb_seuil)] for j in range(nb_col)]
+    best_position_tab = []
+    velocity_tab = [[0.0 for i in range(nb_seuil)] for j in range(50)]
+    g_best_tab = []
+    for element in position_tab:
+        best_position_tab += [element]
+        g_best_tab += [element]
 
     # initialisation ended
 
+    # define an shortcut for gini_entropy
+    def ge(pixel_tab):
+        return gini_entropy.gini_entropy(image, pixel_tab, gray_min, gray_max)
+
+    # function that finds the best position in the whole swarm
+    def g_best_finder(ptab, indice):
+        neighbours = []
+        # collect the neighbours of the particle i j
+        for k in range(indice - 1, indice + 2):
+            if k not in (-1, len(ptab)):
+                neighbours += [ptab[k]]
+        # looking for g_best in the population
+        gbest = neighbours[0]
+        for el in neighbours:
+            if ge(gbest) < ge(element):
+                gbest = element
+        return el
+
+    # find the g best initation
+    g_best = best_position_tab[0]
+    for i in range(1, len(best_position_tab)):
+        if ge(best_position_tab[i]) < ge(g_best):
+            g_best = best_position_tab[i]
     # algorithm  iterations:
 
     k = 0  # iteration init on 0
     psnr_value = 0
 
     # do until psnr index is above 30db or we did a lot of iteration to avoid infinity loop
-    for k in range(20):
-        if k > 0:
-            psnr_value = psnr(image, best_position_tab)
-
+    for k in range(5):
+        # psnr_value = psnr(image, best_position_tab)
         for i in range(nb_col):
-            g_best_tab[i] = g_best_finder(image, i)  # find g_best around the particle i,j
+            #  g_best_tab[i] = g_best_finder(position_tab, i)  # find g_best around the particle i,j
             w = 2 / abs(2 - phi - math.sqrt(phi * (phi - 4)))  # calculating the inert
             c1 = phi1 * w  # calculating momentum coefficient 1
             c2 = phi2 * w  # calculating momentum coefficient 2
-
             # calculate new velocity
             for j in range(nb_seuil):
                 velocity_tab[i][j] = w * velocity_tab[i][j] + (
                         c1 * random() * (best_position_tab[i][j] - position_tab[i][j])) + (
-                                             c2 * random() * (g_best_tab[i][j] - position_tab[i][j]))
+                                             c2 * random() * (g_best[j] - position_tab[i][j]))
                 if velocity_tab[i][j] > 3:  # test if the velocity limit is beyond 3
                     velocity_tab[i][j] = 3
                 elif velocity_tab[i][j] < -3:  # test if the velocity limit is below -3
@@ -102,49 +127,44 @@ def pso(image, nb_region):
 
                 # calculate new position
                 position_tab[i][j] += velocity_tab[i][j]
-            # now checking if the position is in the good borders
+                # now checking if the position is in the good borders
 
                 # test if this is the first seuil
                 if j == 0:
                     if position_tab[i][j] >= position_tab[i][j + 1]:  # test if its above the next seuil
-                        position_tab[i][j] = position_tab[i][j] - 1
+                        position_tab[i][j] = position_tab[i][j+1] /2
                     elif position_tab[i][j] < gray_min:  # test if it's below the gray_min
                         position_tab[i][j] = gray_min
                 # test if this is the last seuil
                 elif j == nb_seuil - 1:
                     if position_tab[i][j] <= position_tab[i][j - 1]:  # test if it's below previous seuil
-                        position_tab[i][j] = position_tab[i][j - 1] + 1
+                        position_tab[i][j] = (position_tab[i][j - 1] + gray_max)/2
                     elif position_tab[i][j] > gray_max:  # test if it's above the gray_max
                         position_tab[i][j] = gray_max
                 # if it's seuil in the middle of the particle we test if it's between the previous and the next sueil
                 else:
-                    if position_tab[i][j] >= position_tab[i][j + 1]:
-                        position_tab[i][j] = position_tab[i][j] - 1
-                    elif position_tab[i][j] <= position_tab[i][j - 1]:
-                        position_tab[i][j] = position_tab[i][j - 1] + 1
+                    position_tab[i][j] = (position_tab[i][j-1] + position_tab[i][j+1]) /2
 
             # update the new personal best position
             if ge(position_tab[i]) < ge(best_position_tab[i]):
                 best_position_tab[i] = position_tab[i]
                 # update the new g_best position if the personal best position is better than g_best
                 if ge(best_position_tab[i]) < ge(g_best_tab[i]):
-                    g_best_tab[i] = best_position_tab[i]
+                    g_best = best_position_tab[i]
 
-        k = k + 1
+        print(k)
     # we pick the optimum solution from the gbest tab
-    optimum = g_best_tab[0]
-    optimum_line = 0
-    for i in range(nb_col):
-        if g_best_tab[i] < optimum:
-            optimum = g_best_tab[i]
-            optimum_line = i
+    optimum = [gray_min] + g_best + [gray_max]
+    print(optimum)
+    draw_image(image, optimum)
     # algorithm ended
     print(k)
     print("optimum : ", optimum)
-    print("index : ", optimum_line)
+
     # new_image = numpy.array(best_position_tab)
     # cv2.imshow("image", new_image)
     # cv2.waitKey()
 
 
-pso(im_gray, 5)
+pso(im_gray, 4)
+
